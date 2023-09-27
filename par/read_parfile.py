8aee23e290bb76d09d26617978c60c95d26ffc1a
Marco Cusinato <marco.cusinato@uv.es>
2023-09-27 11:22:44 +0200

    Added Error tracker and initial parameters reader

--- par/read_parfile.py
+++ par/read_parfile.py
@@ -36,6 +36,8 @@ def get_indices_from_parfile(file_name, path_folder):
         indices['hydro']['I_YE'] = namelist["IINDICES"]["I_YE"] - 1
     if 'I_YZ'in namelist["IINDICES"]:
         indices['hydro']['I_YZ'] = namelist["IINDICES"]["I_YZ"] - 1
+    if 'I_EOSERR'in namelist["IINDICES"]:
+        indices['thd']['I_EOSERR'] = namelist["IINDICES"]["I_EOSERR"] - 1
     if 'I_LRTZ'in namelist["IINDICES"]:
         indices['thd']['I_LRTZ'] = namelist["IINDICES"]["I_LRTZ"] - 1
     if 'I_DENS'in namelist["IINDICES"]:
@@ -109,3 +111,90 @@ def get_indices_from_parfile(file_name, path_folder):
         elif indices['thd'][k1] < 0:
             indices['thd'][k1] = None
     return indices, STENCIL
+
+def get_initial_parameters(path_folder):
+    initial_parameters = {"omgadd": 0.0,
+                          "omgmult": 0.0,
+                          "bfact": 0.0,
+                          "btorfact": 0.0,
+                          "b0": 0.0,
+                          "bt": 0.0
+                          }
+    start_pars = f90nml.read(os.path.join(path_folder, 'start.pars'))
+    if start_pars["PHYSSYST"]["RELATIVISTIC"]:
+        initial_parameters['gravity'] = 'Pseudo-relativistic'
+    else:
+        initial_parameters['gravity'] = 'Newtonian'
+    initial_parameters["gravitational_potential"] = start_pars["GRAVPARS"]["MDPOT"]
+    initial_parameters["lapse_function"] = start_pars["GRAVPARS"]["LAPSE_FORM"]
+    initial_parameters["NS_EOS"] = (os.path.basename(start_pars["SHENEOSPARS"]["SHEN_TBFILE"]))
+    initial_parameters["NS_EOS"] = initial_parameters["NS_EOS"].split('.')[:-1]
+    if type(initial_parameters["NS_EOS"]) == list:
+        if len(initial_parameters["NS_EOS"]) == 1:
+            initial_parameters["NS_EOS"] = initial_parameters["NS_EOS"][0]
+        else:
+            tmp = initial_parameters["NS_EOS"][0]
+            for i in initial_parameters["NS_EOS"][1:]:
+                tmp += '.' + i
+            initial_parameters["NS_EOS"] = tmp
+    
+    files = os.listdir(path_folder)
+    files = [x for x in files if not x == 'start.pars' and not x == '.run']
+    files.sort()
+    Found_heger = False
+    for file in files:
+        namelist = f90nml.read(os.path.join(path_folder, file))
+        if 'Hegerpars' in namelist:
+            Found_heger = True
+            initial_parameters["Heger_model"] = namelist["Hegerpars"]["Heger_model"]
+            if "omgadd" in namelist["Hegerpars"]:
+                initial_parameters["omgadd"] = namelist["Hegerpars"]["omgadd"]
+            if "omgmult" in namelist["Hegerpars"]:
+                initial_parameters["omgmult"] = namelist["Hegerpars"]["omgmult"]
+            if "bfield" in namelist["Hegerpars"]:
+                if namelist["Hegerpars"]["bfield"]:
+                    if "bfact" in namelist["Hegerpars"]:
+                        initial_parameters["bfact"] = namelist["Hegerpars"]["bfact"]
+                    if "btorfact" in namelist["Hegerpars"]:
+                        initial_parameters["btorfact"] = namelist["Hegerpars"]["btorfact"]
+        if "axivecpotpars" in namelist:
+            if "b0" in namelist["axivecpotpars"]:
+                initial_parameters["b0"] = namelist["axivecpotpars"]["b0"]
+            if "bt" in namelist["axivecpotpars"]:
+                initial_parameters["bt"] = namelist["axivecpotpars"]["bt"]
+        if Found_heger:
+            break
+    if not Found_heger:
+        run_path = os.path.join(path_folder, '.run')
+        run_files = os.listdir(run_path).sort()
+        for file in run_files:
+            namelist = f90nml.read(os.path.join(run_path, file))
+            if 'Hegerpars' in namelist and namelist["Hegerpars"]["Heger_model"]:
+                Found_heger = True
+                initial_parameters["Heger_model"] = namelist["Hegerpars"]["Heger_model"]
+                if "omgadd" in namelist["Hegerpars"]:
+                    initial_parameters["omgadd"] = namelist["Hegerpars"]["omgadd"]
+                if "omgmult" in namelist["Hegerpars"]:
+                    initial_parameters["omgmult"] = namelist["Hegerpars"]["omgmult"]
+                if "bfield" in namelist["Hegerpars"]:
+                    if namelist["Hegerpars"]["bfield"]:
+                        if "bfact" in namelist["Hegerpars"]:
+                            initial_parameters["bfact"] = namelist["Hegerpars"]["bfact"]
+                        if "btorfact" in namelist["Hegerpars"]:
+                            initial_parameters["btorfact"] = namelist["Hegerpars"]["btorfact"]
+            if "axivecpotpars" in namelist:
+                if "b0" in namelist["axivecpotpars"]:
+                    initial_parameters["b0"] = namelist["axivecpotpars"]["b0"]
+                if "bt" in namelist["axivecpotpars"]:
+                    initial_parameters["bt"] = namelist["axivecpotpars"]["bt"]
+            if Found_heger:
+                break
+    if not Found_heger:
+        initial_parameters["omgadd"] = None
+        initial_parameters["omgmult"] = None
+        initial_parameters["Heger_model"] = None
+        initial_parameters["bfact"] = None
+        initial_parameters["btorfact"] = None
+        initial_parameters["b0"] = None
+        initial_parameters["bt"] = None
+    return initial_parameters
